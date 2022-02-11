41af2fb6ee8ed4aab05892c7494019d7ac3b41ab
Bruno Hirae <brunodeluca117@outlook.com>
2022-02-11 16:00:24 -0300

    refactor code

--- src/App.js
+++ src/App.js
@@ -6,32 +6,30 @@ import Footer from './components/Footer';
 import GlobalStyles from './styles/GlobalStyles';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
-import usePersistedState from './hooks/usePersistedState';
-import light from './styles/themes/light';
-import dark from './styles/themes/dark';
+import useTheme from './hooks/useTheme';
 
 const App = () => {
-  const [theme, setTheme] = usePersistedState('pokeDataTheme', light)
+	const { theme, toggleTheme } = useTheme();
 
-  const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light)
-  }
-
-  return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Sidebar />
-        <Header theme={theme} toggleTheme={toggleTheme} />
-        <Routes>
-          {routes.map(route => (
-            <Route key={route.name} path={route.path} element={route.element} />
-          ))}
-        </Routes>
-        <Footer />
-      </ThemeProvider>
-    </Router>
-  );
-}
+	return (
+		<Router>
+			<ThemeProvider theme={theme}>
+				<GlobalStyles />
+				<Sidebar />
+				<Header theme={theme} toggleTheme={toggleTheme} />
+				<Routes>
+					{routes.map((route) => (
+						<Route
+							key={route.name}
+							path={route.path}
+							element={route.element}
+						/>
+					))}
+				</Routes>
+				<Footer />
+			</ThemeProvider>
+		</Router>
+	);
+};
 
 export default App;
